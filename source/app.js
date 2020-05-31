101823f5cfb7b3364d11c1363b0c8a69485a92ea
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-06-01 02:28:12 +0530

    Add Pet component

--- source/app.js
+++ source/app.js
@@ -1,12 +1,25 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Sanjana") //second argument
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Sanjana"), //second argument is not gonna be visible due to negative indexing in css check line 94 style.css
+    React.createElement(Pet, {
+      name: "Emo",
+      animal: "Dog",
+      breed: "lebra",
+    }),
+    React.createElement(Pet, {
+      name: "Moana",
+      animal: "Fish",
+      breed: "Star-fish",
+    }),
+    React.createElement(Pet, {
+      name: "Essa",
+      animal: "Cat",
+      breed: "Snowball",
+    }),
+  ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+render(React.createElement(App), document.getElementById("root"));
