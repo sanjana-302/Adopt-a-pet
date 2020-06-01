ab8082688a7fe78ac7d6fe5c6612a2244e4b7acc
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-06-01 21:50:25 +0530

    Add component

---
+++ source/SeachParams.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const SearchParams = () => {
+  const [location, updateLocation] = useState("Seattle, WA"); // useState always returns an array where the first is default location, second is update function
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
