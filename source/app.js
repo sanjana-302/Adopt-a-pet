645fc33decf0febfd3ff168ea901e4c5ec967fa8
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-06-02 21:29:56 +0530

    Add ReactStrict to fix bug

--- source/app.js
+++ source/app.js
@@ -3,10 +3,12 @@ import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
