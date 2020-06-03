365266e6e16e0e689f69c543daae888423282400
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-06-03 23:20:10 +0530

    Modify Details component

--- source/Details.js
+++ source/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
   state = { loading: true };
@@ -21,9 +22,10 @@ class Details extends React.Component {
       return <h1>Loading</h1>;
     }
 
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal}-${breed}-${location}`}</h2>
