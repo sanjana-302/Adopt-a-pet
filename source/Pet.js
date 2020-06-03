1850a9fde749d8d2a743a2374c658023e523ed3b
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-06-03 20:15:09 +0530

    Update Pet component

--- source/Pet.js
+++ source/Pet.js
@@ -1,10 +1,19 @@
 import React from "react";
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let defaulter = "http://placecorgi.com/300/300";
+  if (media.length) {
+    defaulter = media[0].small;
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={defaulter} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 }
