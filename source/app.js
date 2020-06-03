3978d09081b7f04d0b6f72efc1c2470fc0c5b3db
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-06-03 20:16:17 +0530

    Add route to details

--- source/app.js
+++ source/app.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
